88ef001bc9f1630a21fd9201bafa41e2030b3535
Jordan Sinou <sinoujordan@gmail.com>
2022-05-23 15:58:04 +0200

    update controller

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,12 +1,22 @@
-const user_data = require('../data/user_data.json')
+const user_data = require('../data/users.json')
+const fetch = require('node-fetch')
 
 module.exports = {
-    getHome: (req, res) => {
+    getHome: async (req, res) => {
+
+        const data = await fetch('https://jsonplaceholder.typicode.com/todos')
+        const response = await data.json()
+        
+        console.log(response)
+        
+        const users = user_data.filter(user => {
+            return user.age > 25
+        })
 
         res.render('index', {
             title: 'Home',
             username: 'John Doe',
-            users: users
+            users: users,
         })
     },
 }
